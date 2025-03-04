f3e1e78d5ad9b2e8d065de9b7d7059a204556826
sharonrose28 <sharonrose.h53@gmail.com>
2025-03-04 14:22:27 +0530

    Initial commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,8 @@
 const mongoos = require('mongoose');
+const dotenv = require('dotenv');
 const connectDB = async()=>{
     try {
-        await mongoos.connect('mongodb+srv://deadlock:asrasr123@cluster0.mnhdnpb.mongodb.net/?retryWrites=true&w=majority')
+        await mongoos.connect('mongodb+srv://sharonroseh53:D5o7Z5l0AxUk2ouV@cluster0.leodp.mongodb.net/?retryWrites=true&w=majority')
             console.log("Connected!!!")
     } catch (error) {
         console.log("Not Connected!!")
